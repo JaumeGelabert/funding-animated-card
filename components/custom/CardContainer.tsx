93ad0ce421cd4ead1c0903ddbff03254ba1bea78
Jaume Gelabert <gelabertgalmes98@gmail.com>
2024-07-04 17:24:43 +0200

    UI shapping (need to refactor code and organa.)

---
+++ components/custom/CardContainer.tsx
@@ -0,0 +1,182 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { Separator } from "../ui/separator";
+import { Slider } from "../ui/slider";
+
+export const CardContainer = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [arr, setArr] = useState([1]);
+  const [arrValue, setArrValue] = useState("$100k-1M");
+  const [growth, setGrowth] = useState([1]);
+  const [growthValue, setGrowthValue] = useState("10-20%");
+
+  const formatNumber = (value: number): string => {
+    return new Intl.NumberFormat("en-US", {
+      maximumFractionDigits: 0
+    }).format(value);
+  };
+
+  useEffect(() => {
+    switch (arr[0]) {
+      case 0:
+        setArrValue("0-100k");
+        break;
+      case 1:
+        setArrValue("100k-1M");
+        break;
+      case 2:
+        setArrValue("1-10M");
+        break;
+      case 3:
+        setArrValue("10-100M");
+        break;
+      case 4:
+        setArrValue("100-500M");
+        break;
+      case 5:
+        setArrValue("+500M");
+        break;
+      default:
+        break;
+    }
+  }, [arr]);
+
+  useEffect(() => {
+    switch (growth[0]) {
+      case 0:
+        setGrowthValue("0-10%");
+        break;
+      case 1:
+        setGrowthValue("10-20%");
+        break;
+      case 2:
+        setGrowthValue("20-30%");
+        break;
+      case 3:
+        setGrowthValue("30-40%");
+        break;
+      case 4:
+        setGrowthValue("40-50%");
+        break;
+      case 5:
+        setGrowthValue("+50%");
+        break;
+      default:
+        break;
+    }
+  }, [arr]);
+
+  const handleClick = () => {
+    // Animate cards
+    setIsClicked(true);
+    console.log("clicked 1");
+    setTimeout(() => {
+      // Make the cards disappear
+      console.log("clicked 2");
+      setTimeout(() => {
+        // Show result
+        console.log("clicked 3");
+      }, 2000);
+    }, 1000);
+  };
+  return (
+    <div className="p-6 bg-neutral-50 rounded-lg border-neutral-100 border w-[500px] shadow-sm">
+      <span className="flex flex-row justify-between items-center">
+        <p className="font-mono text-neutral-500 text-xs font-medium">
+          Funding round
+        </p>
+        <Link
+          href="https://fifthside.studio"
+          className="font-mono text-neutral-300 text-xs hover:text-neutral-400 transition-colors"
+        >
+          5th Side Studio
+        </Link>
+      </span>
+      <span className="flex flex-col justify-start items-start mt-8 gap-8">
+        <div className="flex flex-col justify-start items-center w-full">
+          <span className="flex flex-row justify-between items-center w-full mb-2">
+            <p className="text-sm">Annual Recurring Revenue</p>
+            <p className="text-xs text-neutral-500 font-mono">{arrValue}</p>
+          </span>
+          <Slider
+            defaultValue={arr}
+            step={1}
+            min={0}
+            max={5}
+            onValueChange={(value) => {
+              setArr(value);
+            }}
+          />
+        </div>
+        <div className="flex flex-col justify-start items-center w-full">
+          <span className="flex flex-row justify-between items-center w-full mb-2">
+            <p className="text-sm">YoY ARR growth </p>
+            <p className="text-xs text-neutral-500 font-mono">$0-3T</p>
+          </span>
+          <Slider
+            defaultValue={growth}
+            step={1}
+            min={0}
+            max={7}
+            onValueChange={(value) => {
+              setGrowth(value);
+            }}
+          />
+        </div>
+      </span>
+      <div className="w-full flex flex-row justify-center items-center my-20 mt-28 relative">
+        <div
+          className={cn(
+            "bg-white w-full max-w-[75%] border rounded-lg p-2 absolute z-20 transition-all duration-400 h-28 flex flex-col justify-start items-start shadow-sm",
+            isClicked ? "opacity-0 scale-75 translate-y-9" : ""
+          )}
+        >
+          <span className="w-full flex flex-row justify-between items-center">
+            <p className="font-mono text-xs text-neutral-500">ASK</p>
+            <p className="font-mono text-xs text-neutral-300">
+              we don't check anything
+            </p>
+          </span>
+          <p className="h-full flex flex-col justify-center w-full text-3xl font-medium text-center">
+            ${formatNumber(arr[0] * growth[0] * 1000)}M
+          </p>
+        </div>
+        <div
+          className={cn(
+            "bg-white w-full max-w-[75%] border rounded-lg p-2 absolute -translate-y-2 z-10 transition-all delay-100 duration-400 h-28 shadow-sm",
+            isClicked ? "scale-75 translate-y-9 opacity-0" : "scale-95"
+          )}
+        >
+          2
+        </div>
+        <div
+          className={cn(
+            "bg-white w-full max-w-[75%] border rounded-lg p-2 absolute -translate-y-4 z-0 scale-90 transition-all delay-1000 duration-400 h-28 shadow-sm",
+            isClicked ? "translate-y-9 opacity-0 scale-75" : "scale-90"
+          )}
+        >
+          1
+        </div>
+      </div>
+      <span>
+        <p className="text-center w-full text-lg font-medium">
+          How much can you get?
+        </p>
+        <div className="w-full px-16 my-2">
+          <Separator />
+        </div>
+        <p className="text-center w-full px-12 text-neutral-500 text-sm">
+          Fund your Startup and never look back. If you don't pay us back, don't
+          worry, we have no lawyers. Just try your best. We will do the same.{" "}
+        </p>
+      </span>
+      <span className="flex flex-row justify-center w-full mt-4">
+        <Button onClick={handleClick}>Calculate</Button>
+      </span>
+    </div>
+  );
+};
---
+++ components/ui/slider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+
+import { cn } from "@/lib/utils";
+
+const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative flex w-full touch-none select-none items-center",
+      className
+    )}
+    {...props}
+  >
+    <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-neutral-100">
+      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+    </SliderPrimitive.Track>
+    <SliderPrimitive.Thumb className="block h-3 w-5 rounded-full border-2 border-primary bg-primary ring-offset-background transition-colors focus-visible:outline-none hover:cursor-grab active:cursor-grabbing  disabled:pointer-events-none disabled:opacity-50" />
+  </SliderPrimitive.Root>
+));
+Slider.displayName = SliderPrimitive.Root.displayName;
+
+export { Slider };
